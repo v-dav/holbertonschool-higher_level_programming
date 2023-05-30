df3cf488b413d309693ba0d842f6f9d1e74ac1d0
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-05-30 09:49:42 +0200

    program that prints the last digit of the number

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+print(f"Last digit of {number} is ", end="")
+if (number < 0):
+    number = -number
+last_number = number % 10
+print(f"{last_number} ", end="")
+if (last_number > 5):
+    print("and is greater than 5")
+elif (last_number == 0):
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
