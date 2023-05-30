61b23459b7167b4e743e8c3fb4afdcc0420ad230
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-05-30 12:50:21 +0200

    fix the function with deleting chr

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,8 @@
 def uppercase(str):
     for i in range(0, len(str)):
         if (ord(str[i]) >= 97 and ord(str[i]) < 123):
-            print("{}".format(chr(ord(str[i]) - 32)), end="")
+            num = 32
         else:
-            print(str[i], end="")
+            num = 0
+        print("{:c}".format(ord(str[i]) - num), end="")
     print()
