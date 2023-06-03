76fe91d567302781b7ed9daf7f6fed49289c45ff
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-03 10:22:52 +0200

    try to fix unknown check

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for i in my_list[::-1]:
-        print("{:d}".format(i))
+    if len(my_list) != 0:
+        for i in my_list[::-1]:
+            print("{:d}".format(i))
+    else:
+        print()
