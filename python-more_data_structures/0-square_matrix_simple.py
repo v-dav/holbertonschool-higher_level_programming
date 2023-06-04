b57b0f67b3c546876905ce047a4ee4d03c6e59cb
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-04 12:35:06 +0200

    try to fix the function

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,2 @@
 def square_matrix_simple(matrix=[]):
-    if matrix:
-        new_matrix = []
-        for i in range(len(matrix)):
-            new_matrix.append([])
-            for j in range(len(matrix[i])):
-                new_matrix[i].append(matrix[i][j] ** 2)
-        return new_matrix
+    return [list(map(lambda x: x ** 2, sublist)) for sublist in matrix]
