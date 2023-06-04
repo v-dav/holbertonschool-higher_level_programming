5984920c003deb68ac1b8b6470fb888516912b69
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-04 12:30:43 +0200

    try to pass the checker

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,9 +2,7 @@ def square_matrix_simple(matrix=[]):
     if matrix:
         new_matrix = []
         for i in range(len(matrix)):
-            new_matrix.append([])  # Ajouter une nouvelle sous-liste pour chaque sous-liste dans matrix
+            new_matrix.append([])
             for j in range(len(matrix[i])):
-                new_matrix[i].append(matrix[i][j] ** 2)  # Ajouter le carré de la valeur à la sous-liste appropriée
+                new_matrix[i].append(matrix[i][j] ** 2)
         return new_matrix
-    else:
-        return matrix  # Si matrix est vide ou None, retourner matrix
