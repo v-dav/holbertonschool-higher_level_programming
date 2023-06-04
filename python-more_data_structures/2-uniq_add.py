6cd2ef87ec7ef96f465771e1080c601ab9a7764f
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-04 14:12:49 +0200

    try to fix unknown check

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if my_list:
-        a = set(my_list)
-        res = 0
-        for number in a:
-            res += number
-        return res
+    a = set(my_list)
+    res = 0
+    for number in a:
+        res += number
+    return res
