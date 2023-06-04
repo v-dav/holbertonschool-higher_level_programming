1722b6146504875972d06a7b943c4744011d003c
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-04 12:36:22 +0200

    try another approach

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,9 @@
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [list(map(lambda x: x ** 2, sublist)) for sublist in matrix]
+    new_matrix = []
+    for i in matrix:
+        list = []
+        for j in i:
+            list.append(j**2)
+        new_matrix.append(list)
+    return new_matrix
