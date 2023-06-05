ad7afe5d88ca8d835082c77b2e5e534dcdec2b21
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-05 22:08:31 +0200

    fix pycodestyle check error

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     number = 0
     if roman_string is None or not isinstance(roman_string, str):
         return number
-    rom = {'I': 1, 'V': 5, 
+    rom = {'I': 1, 'V': 5,
            'X': 10, 'L': 50,
            'C': 100, 'D': 500,
            'M': 1000}
