38449a2666d9e0d2b38f345f043eccd1670307db
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-07 15:22:05 +0200

    remove if condition

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if my_list:
-        try:
-            printed = 0
-            for i in range(x):
-                print("{}".format(my_list[i]), end="")
-                printed += 1
-            print()
-            return printed
-        except IndexError:
-            print()
-            return printed
+    try:
+        printed = 0
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            printed += 1
+        print()
+        return printed
+    except IndexError:
+        print()
+        return printed
