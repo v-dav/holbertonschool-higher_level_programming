c1006d0765d5c7f6c8a6484d3e114464d3769fbc
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-10 20:12:16 +0200

    add docstring to the module and the class

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+"""A mmodule with as simple empty Square class"""
+
+
 class Square:
-    '''An empty class that defines a square'''
+    """An empty class that defines a square"""
