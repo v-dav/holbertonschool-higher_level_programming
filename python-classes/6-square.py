4f410b443180c17327fbfdeb7f2d073f5f8996fd
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-11 14:01:31 +0200

    add check if position is max 2 values

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -27,11 +27,9 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         elif (
-            type(position) is not tuple
-            or type(position[0]) is not int
-            or type(position[1]) is not int
-            or position[0] < 0
-            or position[1] < 0
+            type(position) is not tuple or len(position) < 2
+            or type(position[0]) is not int or type(position[1]) is not int
+            or position[0] < 0 or position[1] < 0
         ):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
@@ -96,11 +94,9 @@ class Square:
     @position.setter
     def position(self, value):
         if (
-            type(value) is not tuple
-            or type(value[0]) is not int
-            or type(value[1]) is not int
-            or value[0] < 0
-            or value[1] < 0
+            type(value) is not tuple or len(value) < 2
+            or type(value[0]) is not int or type(value[1]) is not int
+            or value[0] < 0 or value[1] < 0
         ):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
