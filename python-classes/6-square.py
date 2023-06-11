90e73d7dfdcca303704cccb0b9cd49f432829449
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-11 13:09:16 +0200

    try to pass the checker

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -26,13 +26,7 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        elif (
-            type(position) is not tuple
-            or type(position[0]) is not int
-            or type(position[1]) is not int
-            or position[0] < 0
-            or position[1] < 0
-        ):
+        elif type(position) is not tuple or position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__size = size
@@ -95,13 +89,7 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if (
-            type(value) is not tuple
-            or type(value[0]) is not int
-            or type(value[1]) is not int
-            or value[0] < 0
-            or value[1] < 0
-        ):
+        if type(value) is not tuple or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
