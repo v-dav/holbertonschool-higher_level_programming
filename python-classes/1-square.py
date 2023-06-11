0df1f399cc4fc108558f4bb3e5f2220921e67387
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-11 10:16:29 +0200

    add docstring to the __init__ method

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,4 +5,7 @@
 class Square:
     """A class that defines a square of a given size"""
     def __init__(self, size):
+        """The class __init__ method
+        Args:
+            size (int): The size of the square"""
         self.__size = size
