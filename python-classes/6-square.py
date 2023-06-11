34173498a9be72da56691ffbbb2e8fa15244a8a9
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-11 14:30:24 +0200

    try to pass the checker

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -98,6 +98,8 @@ class Square:
         if self.__size == 0:
             print()
         else:
+            for i in range(self.__position[1]):
+                print()
             for i in range(self.__size):
                 print(" " * self.__position[0], end="")
                 print("#" * self.__size)
