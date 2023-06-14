00b93cbef9d0872b58ffce4cb9d69b3d6ed2f5bc
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-14 12:22:53 +0200

    try to pass the check

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
-"""
-    function to add two int
-"""
+"""A module containing a function that adds 2 integers"""
 
 
 def add_integer(a, b=98):
-    """ function to add two integers """
+    """Add two integers
+
+    Args:
+        a (int or float): first number. Casted to integer if float
+        b (int or float): second number. Casted to integer if float.
+        Optional, by default at 98
+
+    Returns:
+        The result of the addition of two numbers, in integer
+
+    Raises:
+        TypeError: if a or b are not integers or floats
+    """
     try:
-        result = a + b
-        return int(result)
-    except:
+        return int(a + b)
+    except Exception:   
         if type(a) is not int:
             raise TypeError("a must be an integer")
         else:
