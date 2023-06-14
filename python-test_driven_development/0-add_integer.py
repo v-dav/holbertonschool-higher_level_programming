1d0349485c46114214f58caea8fd5c82a2a0651f
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-14 11:13:37 +0200

    test doc case

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-"""A module containing a function that adds 2 integers"""
+"""A module containing a function that adds 2 integers
+
+
+
+"""
 
 
 def add_integer(a, b=98):
     """Add two integers
 
-    Args:
-        a (int or float): first number. Casted to integer if float
-        b (int or float): second number. Casted to integer if float
-
-    Returns:
-        The result of the addition of two numbers, in integer
-
-    Raises:
-        TypeError: if a or b are not integers or floats
     """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
