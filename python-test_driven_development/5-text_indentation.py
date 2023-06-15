f81173230dd3796de2de6ddd8132d0b6ef905db2
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-15 10:48:07 +0200

    test benjmain code

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -17,25 +17,21 @@ def text_indentation(text):
     Raises:
         TypeError: if the text is not string
     """
-
     if type(text) is not str:
         raise TypeError("text must be a string")
 
-    i = 0
-    space = True
-    new_string = ""
+    separators = [".", "?", ":"]
+    result = ""
+    new_line = True
 
-    while i < len(text):
-        if space and text[i] == " ":
-            i += 1
+    for i in text:
+        if new_line and i == " ":
             continue
-        new_string += text[i]
-        if (text[i] == '.' or text[i] == '?' or text[i] == ':')\
-                and i + 1 != len(text):
-            new_string += "\n\n"
-            space = True
-            i += 1
+        result += i
+        if i in separators:
+            result += "\n\n"
+            new_line = True
         else:
-            space = False
-        i += 1
-    print(new_string.strip(), end="")
+            new_line = False
+
+    print(result.strip(), end="")
