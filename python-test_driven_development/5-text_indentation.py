ea0930210f7541af676564cd0ddeec5e70822b4e
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-15 10:22:49 +0200

    fix pycodestyle

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,7 +6,7 @@ def text_indentation(text):
     """
     A function that prints a text with 2 new lines after each
     of these characters: ., ? and :
-    There should be no space at the beginning or at the end of each 
+    There should be no space at the beginning or at the end of each
     printed line.
 
     Args:
