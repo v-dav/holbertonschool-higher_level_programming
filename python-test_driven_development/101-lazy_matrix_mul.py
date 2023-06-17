cec1547a74a5d82398d8c22c8281c288aedaf6db
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-17 10:13:47 +0200

    write doc for the function

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -5,4 +5,13 @@ import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
+    """A function that multiplies two matrices using NumPy module
+
+    Args:
+        m_a (list): first matrix, a list of lists of integers or floats
+        m_b (list): secon matrix, a list of lists of integers or floats
+
+    Returns:
+        a new matrix, result of the multiplication of two matrices
+    """
     return np.matmul(m_a, m_b)
