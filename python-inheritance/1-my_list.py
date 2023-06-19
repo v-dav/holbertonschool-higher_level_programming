e2726789836f051d5c0cd0b542fad2be9888a200
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-19 17:44:36 +0200

    Write a class MyList that inherits from list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A module with a class list"""
+
+
+class MyList(list):
+    """A class list that inherits from list"""
+
+    def print_sorted(self):
+        """A public instance method that prints the list
+        in sorted ascending order
+
+        Args: nothing
+
+        Returns: nothing
+        """
+        new_list = self[:]
+        new_list.sort()
+        print(new_list)
