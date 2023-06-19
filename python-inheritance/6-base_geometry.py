f6e4b6bd99b62471ad21f466961e25f74b43fa4c
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-19 18:45:27 +0200

    Write a class BaseGeometry with a public instance method that raises an Exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A module with a BaseGeometry class"""
+
+
+class BaseGeometry():
+    """An empty class with a non implemented method"""
+
+    def area(self):
+        """A public instance method that raises an Exception
+
+        Args: nothing
+        Returns: nothing
+        Raises: Exception
+        """
+
+        raise Exception("area() is not implemented")
