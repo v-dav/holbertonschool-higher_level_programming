95c475173f48c882b4bfd7a25446e7a2fcfac1c8
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-21 17:00:35 +0200

    write customized to_json instance method

---
+++ python-input_output/10-student.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""A module containing a class Student"""
+
+
+class Student:
+    """A simple Student Class.
+
+    Attributes:
+        first_name (str): Student first name
+        last_name (str): Student last name
+        age (int): Student age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """A Student initialisation contructor method.
+
+        Args:
+            first_name (str): Student first name
+            last_name (str): Student last name
+            age (int): Student age
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """A public instance method that retrieves a dictionary
+        representation of a Student instance.
+
+        Args:
+            attrs (list): a list of strings with attribute names. Optional.
+            Defaults to None.
+
+        Returns: a dictionary representation of a student instance.
+            If attrs is a list of strings, only attribute names contained
+            in this list must be retrieved.
+        """
+
+        if not attrs or type(attrs) is not list:
+            return self.__dict__
+
+        select_dict = {}
+        for attr in attrs:
+            if type(attr) is not str:
+                return self.__dict__
+            else:
+                if attr in self.__dict__:
+                    select_dict[attr] = self.__dict__[attr]
+        return select_dict
