fda9eaae931e039ff17abd2744b68b9d0ae77ef2
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-21 10:02:52 +0200

    use self instead of super

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -17,7 +17,7 @@ class Rectangle(BaseGeometry):
 
         Returns: nothing
         """
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
