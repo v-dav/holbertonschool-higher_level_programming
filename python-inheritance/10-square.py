aa1bf093085f9a50c1c13ef75d411989549e4475
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-21 10:56:59 +0200

    try to pass the checker

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-"""Creating a class: Sqaure"""
+"""A module with the class square"""
 
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """def __init__"""
-    def __init__(self, size):
-        self.integer_validator("size", size)
-        self.__size = size
+    """A class representing a square"""
 
-    def area(self):
-        return (self.__size*self.__size)
+    def __init__(self, size):
+        """A constructor method that initializes a new square object
+        with private instance attribute after validation that the value
+        is a positive integer
+
+        Args:
+            size (int): the size of the Rectangle
+
+        Returns: nothing
+        """
+        super().__init__(size, size)
+        self.__size = size
