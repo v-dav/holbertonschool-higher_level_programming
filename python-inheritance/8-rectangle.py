1894872d94d74fb938d810cd3a03643b6db6597a
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-21 10:04:09 +0200

    why the checker doesn't want

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
-"""A module with a Rectangle class"""
+"""Creating a class: Rectangle"""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """def __init__"""
+
     def __init__(self, width, height):
-        """A constructor method that initializes a new rectangle object
-        with private instance attributes after validation that the values
-        are positive integers
-
-        Args:
-            width (int): the width of the Rectangle
-            height (int): the height of the Rectangle
-
-        Returns: nothing
-        """
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
