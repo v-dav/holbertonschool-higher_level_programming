668fb53dcd392abe2465aba449739256cb6b6837
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-21 15:59:10 +0200

    factorize the script to make it shorter

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,12 +8,10 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-args_list = sys.argv[1:]
-
 try:
     json_list = load_from_json_file("add_item.json")
-    for i in range(len(args_list)):
-        json_list.append(args_list[i])
+    for item in sys.argv[1:]:
+        json_list.append(item)
     save_to_json_file(json_list, "add_item.json")
 except Exception:
-    save_to_json_file(args_list, "add_item.json")
+    save_to_json_file(sys.argv[1:], "add_item.json")
