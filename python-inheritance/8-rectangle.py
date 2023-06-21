621be1049bf5625fd058338669c651c929a0feca
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-21 10:06:20 +0200

    add docstring to the geometry class

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
-"""Creating a class: Rectangle"""
+"""A module with a Rectangle class"""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """def __init__"""
+    """A rectangle class heriting from BaseGeometry"""
 
     def __init__(self, width, height):
+        """A constructor method that initializes a new rectangle object
+        with private instance attributes after validation that the values
+        are positive integers
+
+        Args:
+            width (int): the width of the Rectangle
+            height (int): the height of the Rectangle
+
+        Returns: nothing
+        """
         self.integer_validator("width", width)
-        self.__width = width
         self.integer_validator("height", height)
+        self.__width = width
         self.__height = height
