209dd29e04ccd0c2bb7b22f31c5e4435b4ecc24f
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-21 13:43:44 +0200

    remove the if condition in the function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -11,6 +11,5 @@ def read_file(filename=""):
 
     Returns: nothing
     """
-    if filename != "":
-        with open(filename, encoding="utf-8") as f:
-            print(f.read())
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
