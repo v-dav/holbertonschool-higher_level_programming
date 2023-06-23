72a64a2ddc670c5519b13666739c771205034a85
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-23 13:17:01 +0200

    try to pass the checker

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,37 +1,34 @@
 #!/usr/bin/python3
-"""A script that reads stdin line by line and computes metrics"""
+"""Module contain reads stdin line by line and computes metric"""
 
 
 import sys
 
-counter = 0
-total_size = 0
-status_code_list = ["200", "301", "400", "401", "403", "404", "405", "500"]
+status_code = ["200", "301", "400", "401", "403", "404", "405", "500"]
 metrics = {}
-
-
-def print_stats():
-    """
-    The function prints the total file size and a count of each status code.
-    """
-    print("File size: {}".format(total_size))
-    for key, value in sorted(metrics.items()):
-        print(f"{key}: {value}")
-
+total_size = 0
+line_cnt = 0
 
 try:
-    try:
-        for line in sys.stdin:
-            counter += 1
+    for line in sys.stdin:
+        if line_cnt == 10:
+            print(f"File size: {total_size}")
+            for key, value in sorted(metrics.items()):
+                print(f"{key}: {value}")
+            line_cnt = 1
+
+        try:
             status = line.split()[-2]
-            if status in status_code_list:
+            if status in status_code:
                 total_size += int(line.split()[-1])
                 metrics[status] = metrics.get(status, 0) + 1
-            if counter % 10 == 0:
-                print_stats()
-    except Exception:
-        pass
+        except Exception:
+            pass
+
+        line_cnt += 1
 
 except KeyboardInterrupt:
-    print_stats()
+    print(f"File size: {total_size}")
+    for key, value in sorted(metrics.items()):
+        print(f"{key}: {value}")
     raise
