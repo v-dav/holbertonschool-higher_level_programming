c85e977cc8393337277d920df1574e5f52f1f5b6
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-28 11:02:22 +0200

    write tests for to_json_string static method of the class

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -2,6 +2,8 @@
 
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+import json
 
 
 class TestBaseClass(unittest.TestCase):
@@ -9,6 +11,7 @@ class TestBaseClass(unittest.TestCase):
 
     def test_object_noId(self):
         """Test object creation without providing an ID"""
+
         self.b1 = Base()
         self.b2 = Base()
         self.assertEqual(self.b1.id, 1)
@@ -16,6 +19,7 @@ class TestBaseClass(unittest.TestCase):
 
     def test_object_withId(self):
         """Test object creation while providing an ID"""
+
         self.b3 = Base(146)
         self.b4 = Base(0)
         self.b5 = Base(-5)
@@ -24,3 +28,17 @@ class TestBaseClass(unittest.TestCase):
         self.assertEqual(self.b3.id, 146)
         self.assertEqual(self.b5.id, -5)
         self.assertEqual(self.b6.id, 1000000)
+
+    def test_toJsonString(self):
+        """Test the static method to_json_string"""
+
+        self.r1 = Rectangle(10, 7, 2, 8)
+        dictionary = self.r1.to_dictionary()
+
+        json_list_of_dict = Base.to_json_string([dictionary])
+        revert_from_json = json.loads(json_list_of_dict)
+
+        a_list_of_dict = []
+        a_list_of_dict.append(dictionary)
+
+        self.assertEqual(a_list_of_dict, revert_from_json)
