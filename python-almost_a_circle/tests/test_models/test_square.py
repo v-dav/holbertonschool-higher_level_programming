0684d31992d2009ef0e365650b856bdc38f73fb7
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-06-28 09:25:05 +0200

    update tests for update method of the square class

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -111,12 +111,12 @@ class TestSquareClass(unittest.TestCase):
         self.r1.update(89, 3, 4, 5)
         with patch("sys.stdout", new=io.StringIO()) as output:
             print(self.r1)
-            expected_output = "[Square] (89) 5/10 - 3\n"
+            expected_output = "[Square] (89) 4/5 - 3\n"
             self.assertEqual(output.getvalue(), expected_output)
 
         """Test with **kwargs"""
-        self.r1.update(x=1, height=2, y=3, width=4)
+        self.r1.update(size=7, id=100, y=1)
         with patch("sys.stdout", new=io.StringIO()) as output:
             print(self.r1)
-            expected_output = "[Square] (89) 1/3 - 4\n"
+            expected_output = "[Square] (100) 4/1 - 7\n"
             self.assertEqual(output.getvalue(), expected_output)
