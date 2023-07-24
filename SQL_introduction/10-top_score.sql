775a93ec2647cbfb4d4c7a486c5fd42be71bc257
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-24 18:50:19 +0200

    change comments syntax

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,5 +1,4 @@
-/* a script that lists all records of the table second_table of the database hbtn_0c_0
-in my MySQL server in descending order */
+-- a script that lists all records of the table second_table of the database hbtn_0c_0 in my MySQL server in descending order
 
 SELECT second_table.score, second_table.name
 FROM second_table
