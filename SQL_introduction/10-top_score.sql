86d9c2a45787c1a418117f4dd7d7748c07dafcc4
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-24 18:48:50 +0200

    a script that lists all records of the table second_table of the database hbtn_0c_0 in my MySQL server in descending order

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+/* a script that lists all records of the table second_table of the database hbtn_0c_0
+in my MySQL server in descending order */
+
+SELECT second_table.score, second_table.name
+FROM second_table
+ORDER BY score DESC;
