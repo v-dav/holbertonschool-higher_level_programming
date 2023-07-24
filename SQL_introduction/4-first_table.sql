c3dc4e904e4ef8f418cb8a455019ec4207455ddc
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-24 18:18:51 +0200

    remove primary key attribute

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- a script that creates a table called first_table in the current database in my MySQL server
 
 CREATE TABLE IF NOT EXISTS first_table(
-	id INT PRIMARY KEY,
+	id INT,
 	name VARCHAR(256)
 );
