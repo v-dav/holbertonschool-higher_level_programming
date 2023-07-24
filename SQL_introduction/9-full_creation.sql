d4f68cdc17eee1b2a7f7e953d42c2d6b6504b51a
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-24 18:39:15 +0200

    fix typo

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -6,7 +6,8 @@ CREATE TABLE IF NOT EXISTS second_table (
 	score INT
 );
 
-INSERT INTO second_table VALUES(1, "John", 10), 
+INSERT INTO second_table VALUES
+(1, "John", 10), 
 (2, "Alex", 3), 
 (3, "Bob", 14), 
 (1, "George", 8);
