43fd447aef3a3324b6cefb57c052c6cc768f4c36
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-25 14:51:05 +0200

    fix syntax for the checker

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
 -- a script that creates the MySQL server user user_0d_1 with all privileges and password
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.*
+TO 'user_0d_1'@'localhost';
