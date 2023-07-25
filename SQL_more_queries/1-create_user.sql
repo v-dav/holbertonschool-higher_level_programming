728d266e69d6ff1e059cdb1de7e7561e99e56352
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-25 14:52:48 +0200

    try to fix unknown checker errors

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- a script that creates the MySQL server user user_0d_1 with all privileges and password
+-- MYSQL script that creates the MySQL server user user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.*
