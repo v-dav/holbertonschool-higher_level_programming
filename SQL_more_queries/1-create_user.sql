52ec75ed845fb96ad4eea50a72747b1c8ef8d897
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-25 15:02:39 +0200

    tryx to fix 2 checks

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,6 @@
 -- MYSQL script that creates the MySQL server user user_0d_1
+-- with password and grants all privileges
+
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.*
