6d1d0ea759494edb9510737abe019588aa91797c
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-26 10:26:08 +0200

    add alias to the column name

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- a script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
-SELECT tv_genres.name
+SELECT tv_genres.name AS name
 FROM tv_genres
 RIGHT JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 WHERE show_id = 8
-ORDER BY tv_genres.name;
+ORDER BY name;
