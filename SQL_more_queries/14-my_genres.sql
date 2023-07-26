088a05553b27a2ece8d8fe469ca00af6d12267bf
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-26 10:40:46 +0200

    try to pass the checker with one join

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- a script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
-SELECT tv_genres.name AS name
+SELECT tv_genres.name
 FROM tv_genres
 RIGHT JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 WHERE show_id = 8
-ORDER BY name;
+ORDER BY tv_genres.name ASC;
