a03156e1f4f6d25207e6bc425ffd4ed71d835379
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-26 10:46:32 +0200

    made double join because show id can be different

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- a script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
 SELECT tv_genres.name
 FROM tv_genres
-RIGHT JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id
-WHERE show_id = 8
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE title = "Dexter"
 ORDER BY tv_genres.name;
