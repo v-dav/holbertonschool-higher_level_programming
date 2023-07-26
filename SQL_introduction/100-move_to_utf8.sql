c6e7c59ca8ffb6a8fdd20c72a351c97b56f7652d
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-26 11:36:42 +0200

    try to pass the checker

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,15 +1,17 @@
--- a script that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in my MySQL server.
+-- a script that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in my MySQL server
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
+USE hbtn_0c_0;
+
 -- a part of the script that converts table first_table to UTF8
-ALTER TABLE first_table
+ALTER TABLE hbtn_0c_0.first_table
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
 -- converts the field name of first table to UTF8
-ALTER TABLE first_table
+ALTER TABLE hbtn_0c_0.first_table
 MODIFY COLUMN name VARCHAR(256) 
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
