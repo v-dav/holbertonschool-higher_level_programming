d3c64368f4e06177a8e7b4f4c4b807582496e655
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-29 17:39:37 +0200

    Write a script that lists all states from the database hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """A script that lists all states from the database hbtn_0e_0_usa"""
 
-import sys
-print(sys.path)
-
 import MySQLdb
 
 if __name__ == "__main__":
@@ -11,5 +8,8 @@ if __name__ == "__main__":
                          passwd="root", db="hbtn_0e_0_usa")
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
     cur.close()
     db.close()
