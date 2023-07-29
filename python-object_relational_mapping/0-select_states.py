03276c5ebc492361d3f6fcb54fc11d350dc5bbe0
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-29 17:50:14 +0200

    add sysargv 3

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db="hbtn_0e_0_usa")
+        db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id")
     rows = cur.fetchall()
