3a17beec109fc97f23199eefed85c9fb9ae57d05
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-30 09:12:36 +0200

    add LIKE BINARY because case sensitive

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     cursor.execute(
         """SELECT *
         FROM states
-        WHERE name LIKE 'N%'
+        WHERE name LIKE BINARY 'N%'
         ORDER BY states.id""")
 
     rows = cursor.fetchall()
