0eab78896501b8811a63ddfc7911a683d8528669
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-30 12:51:50 +0200

    fix the file

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,12 +2,8 @@
 """A python file that contains the class definition of a State
 and an instance Base = declarative_base()"""
 
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-import sys
-
-engine = create_engine("mysql://{}:{}@localhost:3306/{}"
-                       .format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
 Base = declarative_base()
 
