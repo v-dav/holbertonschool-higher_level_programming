d627c95214bbc74014412923875e9f613eeb75b7
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-31 10:53:27 -0700

    try to pass the checker

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -2,22 +2,24 @@
 """a script that lists all State objects, and corresponding City objects,
 contained in the database hbtn_0e_101_usa"""
 
-import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import State
+import sys
+from relationship_state import Base, State
 from relationship_city import City
 
 if __name__ == "__main__":
+    """Create an engine"""
     engine = create_engine("mysql://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
+    """Create a session"""
     Session = sessionmaker(bind=engine)
     session = Session()
 
     for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
         for city in sorted(state.cities, key=lambda city: city.id):
-            print("    {}: {}".format(city.id, city.name))
+            print("\t{}: {}".format(city.id, city.name))
 
     session.close()
