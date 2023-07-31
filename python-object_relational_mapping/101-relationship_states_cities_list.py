bf1b60c9694271c187558323c7f1fa3ba7a6b2ed
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-31 11:01:20 -0700

    add all method to the query

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id):
+    for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
         for city in sorted(state.cities, key=lambda city: city.id):
             print("    {}: {}".format(city.id, city.name))
